ac77d36a75d20c0e573de3d9192d4196878bf2d9
igorv <i.voitovych@liga.net>
2026-05-06 21:12:42 +0300

    docs: add @vueuse/core to ssr.noExternal for VitePress 2

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       },
     },
     ssr: {
-      noExternal: ['vuetify'],
+      noExternal: ['vuetify', '@vueuse/core'],
     },
     optimizeDeps: {
       include: ['vuetify', '@vueuse/core'],
